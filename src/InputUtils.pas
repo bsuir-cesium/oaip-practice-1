db3e81f0c71cf9e64dfe26757ef9cad8a9d447a2
MarkCesium <danikhrenkov29@gmail.com>
2025-04-14 18:03:51 +0300

    Add edit procedures

--- src/InputUtils.pas
+++ src/InputUtils.pas
@@ -9,6 +9,9 @@ procedure AddNewVacancy(var VacanciesHead: PVacancyNode;
   CompaniesHead: PCompanyNode);
 procedure AddNewCandidate(var CandidatesHead: PCandidateNode);
 procedure AddNewCompany(var CompaniesHead: PCompanyNode);
+procedure EditVacancy(VacanciesHead: PVacancyNode; ID: Integer);
+procedure EditCandidate(CandidatesHead: PCandidateNode; ID: Integer);
+procedure EditCompany(CompaniesHead: PCompanyNode; ID: Integer);
 
 implementation
 
@@ -104,6 +107,98 @@ begin
   Write('Компания ' + NewCompany.Name + ' с ID: ');
   Write(NewCompany.ID);
   Writeln(' была успешно создана!');
+end;
+
+procedure EditVacancy(VacanciesHead: PVacancyNode; ID: Integer);
+var
+  isFinded: Boolean;
+begin
+  isFinded := False;
+
+  while VacanciesHead <> nil do
+  begin
+    if VacanciesHead^.Data^.ID = ID then
+    begin
+      isFinded := True;
+      Write('Специальность: ');
+      Readln(VacanciesHead^.Data^.Specialty);
+      Write('Должность: ');
+      Readln(VacanciesHead^.Data^.Position);
+      Write('Оклад: ');
+      Readln(VacanciesHead^.Data^.Salary);
+      Write('Дней отпуска: ');
+      Readln(VacanciesHead^.Data^.VacationDays);
+      VacanciesHead^.Data^.RequiresHigherEducation :=
+        ReadBoolean('Требуется высшее образование (1-Да/0-Нет): ');
+      Write('Минимальный возраст: ');
+      Readln(VacanciesHead^.Data^.MinAge);
+      Write('Максимальный возраст: ');
+      Readln(VacanciesHead^.Data^.MaxAge);
+    end;
+    VacanciesHead := VacanciesHead^.Next;
+  end;
+
+  if not isFinded then
+    Writeln('Вакансия с ID: ', ID, ' не найдена.');
+end;
+
+procedure EditCandidate(CandidatesHead: PCandidateNode; ID: Integer);
+var
+  isFinded: Boolean;
+  DateStr: string;
+  DateValue: TDateTime;
+begin
+  isFinded := False;
+
+  while CandidatesHead <> nil do
+  begin
+    if CandidatesHead^.Data^.ID = ID then
+    begin
+      isFinded := False;
+      Write('ФИО кандидата: ');
+      Readln(CandidatesHead^.Data^.FullName);
+      repeat
+        Write('Дата рождения (дд.мм.гггг): ');
+        Readln(DateStr);
+      until TryStrToDate(DateStr, DateValue);
+      CandidatesHead^.Data^.BirthDate := DateValue;
+
+      Write('Специальность: ');
+      Readln(CandidatesHead^.Data^.Specialty);
+      CandidatesHead^.Data^.HasHigherEducation :=
+        ReadBoolean('Наличие высшего образования (1-Да/0-Нет): ');
+      Write('Желаемая должность: ');
+      Readln(CandidatesHead^.Data^.DesiredPosition);
+      Write('Минимальный оклад: ');
+      Readln(CandidatesHead^.Data^.MinSalary);
+    end;
+    CandidatesHead := CandidatesHead^.Next;
+  end;
+
+  if not isFinded then
+    Writeln('Кандидат с ID: ', ID, ' не найден.');
+end;
+
+procedure EditCompany(CompaniesHead: PCompanyNode; ID: Integer);
+var
+  isFinded: Boolean;
+begin
+  isFinded := False;
+
+  while CompaniesHead <> nil do
+  begin
+    if CompaniesHead^.Data^.ID = ID then
+    begin
+      isFinded := True;
+      Write('Название компании: ');
+      Readln(CompaniesHead^.Data^.Name);
+    end;
+    CompaniesHead := CompaniesHead^.Next;
+  end;
+
+  if not isFinded then
+    Writeln('Компания с ID: ', ID, ' не найдена.');
+
 end;
 
 end.
